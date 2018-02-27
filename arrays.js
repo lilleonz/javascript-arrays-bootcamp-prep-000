7079f384e7620eee99401957473f3c2dfaf6ed5b
Leon Little <little-leon@hotmail.com>
2018-02-27 03:12:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,4 +9,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element):
+  return array;
+}
