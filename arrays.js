2561971596596320a6e39f13eb8378197120abda
Leon Little <little-leon@hotmail.com>
2018-02-27 03:09:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = [
-  'snickers',
-  'hundred grand',
-  'kitkat',
-  'skittles'
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
 ];
 
 var addElementToBeginningOfArray(array, element) {
